22b036bea8656401b9c0913c1a2dc5416736931a
saeed-abb <abbasi.sa77@gmail.com>
2019-01-28 12:12:30 +0330

    update api get_logged_in_user_id to return user_type

--- web/api/views/get_logged_in_user_id.py
+++ web/api/views/get_logged_in_user_id.py
@@ -1,10 +1,13 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
+from insta.db.database import get_user_type_query
 
 
 @require_GET
 def get_logged_in_user_id(request):
     user_id = None
+    user_type = None
     if request.session.session_key in request.session:
         user_id = request.session[request.session.session_key]
-    return JsonResponse({'id': user_id}, status=200)
+        user_type = get_user_type_query(user_id=user_id)
+    return JsonResponse({'id': user_id, 'user_type': user_type}, status=200)
